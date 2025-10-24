5effbf8cbf0761e12680d283e168db966d675eed
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-10-24 17:59:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7016189097-89342",
+  "appId": "1:1022805206009:web:97a81fe4602d1c3dbed328",
+  "apiKey": "AIzaSyDnQd5Tti-a-weyYjnN0ISxD6kjtxX8e60",
+  "authDomain": "studio-7016189097-89342.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1022805206009"
+};
